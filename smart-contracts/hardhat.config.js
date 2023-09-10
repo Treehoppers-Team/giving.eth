7f79bc31a364db9cf254c96aee3712648dae062b
rilwan99 <rilwanwild@gmail.com>
2023-09-10 18:22:05 +0800

    update hardhat config

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -3,4 +3,16 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "matic", 
+  networks: {
+    hardhat: {
+    }, 
+    matic: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY]
+    }, 
+    etherscan: {
+      apiKey: POLYGONSCAN_API_KEY
+    }
+  }
 };
