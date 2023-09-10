9f29cfd3b0a5959eeb331610e269eec1b2f03506
rilwan99 <rilwanwild@gmail.com>
2023-09-10 18:34:04 +0800

    update hardhat config

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -5,6 +5,12 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+// Env variables
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
@@ -23,6 +29,9 @@ async function main() {
       lockedAmount
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
   );
+
+  const simpleAccount = await hre.ethers.deployContract("SimpleAccount", ["0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"]);
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
