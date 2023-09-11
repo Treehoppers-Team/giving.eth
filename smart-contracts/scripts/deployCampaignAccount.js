b5b65ba43ce9273e34e4c9824181499694615b1d
AvranWong <avranwong@gmail.com>
2023-09-11 10:15:57 +0800

    Set up CampaignAccountWallets

--- smart-contracts/scripts/deployCampaignAccount.js
+++ smart-contracts/scripts/deployCampaignAccount.js
@@ -5,25 +5,26 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 
-// npx hardhat run scripts/deploySimpleAccount.js --network matic   
-// npx hardhat verify --network matic <SIMPLE_ACCOUNT_FACTORY_ADDRESS> 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
+// npx hardhat run scripts/deployCampaignAccount.js --network matic   
+// npx hardhat verify --network matic <SIMPLE_ACCOUNT_ADDRESS> 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
 const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-
   // Replace with the address of the EntryPoint contract you want to use
   const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 
-  const SimpleAccounyFactory = await hre.ethers.deployContract("SimpleAccountFactory",[entryPointAddress]);
+  const CampaignAccount = await hre.ethers.deployContract("CampaignAccount",[entryPointAddress]);
 
-  await SimpleAccounyFactory.waitForDeployment();
+  await CampaignAccount.waitForDeployment();
 
-  console.log(`SimpleAccountFactory deployed to: ${SimpleAccounyFactory.target}`);
+  console.log(`CampaignAccount deployed to: ${CampaignAccount.target}`);
 }
+
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
--- smart-contracts/scripts/deployCampaignAccountFactory.js
+++ smart-contracts/scripts/deployCampaignAccountFactory.js
@@ -5,26 +5,25 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 
-// npx hardhat run scripts/deploySimpleAccount.js --network matic   
-// npx hardhat verify --network matic <SIMPLE_ACCOUNT_ADDRESS> 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
+// npx hardhat run scripts/deployCampaignAccountFactory.js --network matic
+// npx hardhat verify --network matic <SIMPLE_ACCOUNT_FACTORY_ADDRESS> 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789
 const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
+
   // Replace with the address of the EntryPoint contract you want to use
   const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 
-  const SimpleAccount = await hre.ethers.deployContract("SimpleAccount",[entryPointAddress]);
+  const CampaignAccountFactory = await hre.ethers.deployContract("CampaignAccountFactory",[entryPointAddress]);
 
-  await SimpleAccount.waitForDeployment();
+  await CampaignAccountFactory.waitForDeployment();
 
-  console.log(`SimpleAccount deployed to: ${SimpleAccount.target}`);
+  console.log(`CampaignAccountFactory deployed to: ${CampaignAccountFactory.target}`);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
