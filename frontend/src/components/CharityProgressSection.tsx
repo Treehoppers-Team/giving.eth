942806d2753f13ab355f731ffaa8a7252744e674
rilwan99 <rilwanwild@gmail.com>
2023-09-11 14:59:19 +0800

    update donation button

--- frontend/src/components/CharityProgressSection.tsx
+++ frontend/src/components/CharityProgressSection.tsx
@@ -14,15 +14,9 @@ interface CharityProgressSectionProps {
 
 const submitTransaction = async () => {
   try {
-    // const web3Provider = new Web3.providers.HttpProvider(
-    //   process.env.NEXT_PUBLIC_PROVIDERURL
-    // );
-    // const web3 = new Web3(web3Provider);
-    const web3 = new Web3(window.ethereum);
 
-    // Check if Ethereum provider (MetaMask) is available
+    const web3 = new Web3(window.ethereum);
     if (window.ethereum) {
-      // Request account access from the user
       await window.ethereum.request({ method: "eth_requestAccounts" });
 
       // Get the user's Ethereum account
@@ -31,7 +25,9 @@ const submitTransaction = async () => {
       console.log("Sender Address:", senderAddress);
 
       // Recipient's address (null address)
-      const recipientAddress = "0x0000000000000000000000000000000000000000";
+      const response = await fetch('/api/blockchain')
+      const recipientAddress = await response.json()
+      console.log("Recipient Address:", recipientAddress);
 
       // Amount to transfer (0.000001 MATIC in Wei)
       const amountInWei = web3.utils.toWei("0.000000000000000001", "ether");
@@ -41,7 +37,7 @@ const submitTransaction = async () => {
         from: senderAddress,
         to: recipientAddress,
         value: amountInWei,
-        chainId: 8001,
+        chainId: 80001,
       };
 
       console.log("Transaction Object:", transactionObject);
