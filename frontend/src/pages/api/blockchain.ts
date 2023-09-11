4c82fc3725e126f1628ef506ac6359db084bb74d
AvranWong <avranwong@gmail.com>
2023-09-11 14:36:46 +0800

    populate 4337 methods

--- frontend/src/pages/api/blockchain.ts
+++ frontend/src/pages/api/blockchain.ts
@@ -1,5 +1,5 @@
-import { ethers } from "ethers";
-import { campaignAccountFactoryABI } from "../../abis/abi"
+import { BigNumber, ethers } from "ethers";
+import { campaignAccountFactoryABI, campaignAccountABI } from "../../abis/abi"
 import { getFirestore } from "firebase/firestore";
 import firebaseApp from "../../../firebaseConfig"
 import { NextApiRequest, NextApiResponse } from "next";
@@ -24,21 +24,166 @@ export default async function handler(
 
 
 /** ERC4337 METHODS **/
+interface UserOperation {
+  Sender: string;
+  Nonce: BigNumber;
+  InitCode: Uint8Array;
+  CallData: string;
+  CallGasLimit: BigNumber;
+  VerificationGasLimit: BigNumber;
+  PreVerificationGas: BigNumber;
+  MaxFeePerGas: BigNumber;
+  MaxPriorityFeePerGas: BigNumber;
+  PaymasterAndData: Uint8Array;
+  Signature: string;
+}
 
 // BuildAndSignOp needs to have BuildOp method, getUserOpHash and Sign
 // then Append the signature to the Op
+async function buildAndSignOp(
+  salt: string,
+  sender: string,
+  owner: string,
+  factory: string,
+  target: string,
+  value: BigNumber,
+  nonce: BigNumber,
+  targetData: Uint8Array,
+  suppliers: string[],
+): Promise<UserOperation> {
+  const op = buildOp(
+    stringToBigInt(salt),
+    sender,
+    factory,
+    owner,
+    target,
+    value,
+    nonce,
+    targetData,
+    suppliers
+  );
+  const opHash = getUserOpHash(op, "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", BigNumber.from(5));
+  console.log("Op Hash:", opHash.toString());
+
+  const signingPkey = process.env.SIGNING_PKEY;
+  const signature = await signEIP191(opHash, signingPkey);
+
+  const opSigned: UserOperation = {
+    ...op,
+    Signature: signature,
+  };
+
+  return opSigned;
+}
+
+function stringToBigInt(str: string): BigNumber {
+  return BigNumber.from(str);
+}
+
+async function signEIP191(inputData: string, signingKey: any): Promise<string> {
+  const signingKeyBytes = ethers.utils.arrayify(signingKey);
+  const privateKey = new Uint8Array(signingKeyBytes.slice(2)); // Remove the "0x" prefix
+  let wallet = new ethers.Wallet(privateKey);
+  const hashBytes = ethers.utils.arrayify(inputData);
+  const signature = await wallet.signMessage(hashBytes);
+
+  return signature;
+}
+
+function getUserOpHash(op: UserOperation, entryPoint: string, chainId: BigNumber) {
+  const packed = ethers.utils.defaultAbiCoder.encode(
+    [
+      "address",
+      "uint256",
+      "bytes32",
+      "bytes32",
+      "uint256",
+      "uint256",
+      "uint256",
+      "uint256",
+      "uint256",
+      "bytes32",
+    ],
+    [
+      op.Sender,
+      op.Nonce,
+      ethers.utils.keccak256(op.InitCode),
+      ethers.utils.keccak256(op.CallData),
+      op.CallGasLimit,
+      op.VerificationGasLimit,
+      op.PreVerificationGas,
+      op.MaxFeePerGas,
+      op.MaxPriorityFeePerGas,
+      ethers.utils.keccak256(op.PaymasterAndData),
+    ]
+  );
+
+  const enc = ethers.utils.defaultAbiCoder.encode(
+    ["bytes32", "address", "uint256"],
+    [ethers.utils.keccak256(packed), entryPoint, chainId]
+  );
+
+  return ethers.utils.keccak256(enc);
+}
+
 
 // BuildOp needs to generate the Generate4337Initcode, and BuilcOpCallData
+function buildOp(
+  salt: BigNumber,
+  sender: string,
+  factory: string,
+  owner: string,
+  target: string,
+  value: BigNumber,
+  nonce: BigNumber,
+  targetData: Uint8Array,
+  suppliers: string[]
+): UserOperation {
+  let initCode: Uint8Array = new Uint8Array();
+  if (nonce.eq(BigNumber.from(0))) {
+    initCode = generate4337Initcode(factory, owner, salt, suppliers );
+  }
+  const campaignAccountInterface = new ethers.utils.Interface(campaignAccountABI);
+  const callData: string = buildCallData(campaignAccountInterface, "execute", target, targetData);
+
+  const op: UserOperation = {
+    Sender: sender,
+    Nonce: nonce,
+    InitCode: initCode,
+    CallData: callData,
+    CallGasLimit: BigNumber.from(200000),
+    VerificationGasLimit: BigNumber.from(1000000),
+    PreVerificationGas: BigNumber.from(50000),
+    MaxFeePerGas: BigNumber.from(100000000000),
+    MaxPriorityFeePerGas: BigNumber.from(1000000000),
+    PaymasterAndData: new Uint8Array(), // Replace with the appropriate value
+    Signature: ""
+  };
+
+  return op;
+}
+// BuildCallData
+function buildCallData(contractInterface: ethers.utils.Interface, functionName: string, ...args: any[]): string {
+  try {
+    // Encode the function data
+    const data = contractInterface.encodeFunctionData(functionName, args);
+
+    return data;
+  } catch (error) {
+    console.error(`Failed to encode function data: ${error}`);
+    throw error;
+  }
+}
 
 // GenerateInitCode
-function generate4337Initcode(factoryAddress: string, owner: string, salt: bigint, suppliers: string[]): Uint8Array {
+function generate4337Initcode(factory: string, owner: string, salt: BigNumber, suppliers: string[]): Uint8Array {
     const parsedABI = new ethers.utils.Interface(campaignAccountFactoryABI);
 
     // Encode the function data
     const data = parsedABI.encodeFunctionData("createAccount", [owner, salt, suppliers]);
 
     // Convert factory address and data to bytes
-    const factoryAddressBytes = ethers.utils.arrayify(factoryAddress);
+    const factoryAddressBytes = ethers.utils.arrayify(factory);
     const concatBytes = ethers.utils.concat([factoryAddressBytes, data]);
 
     console.log("InitCode:", ethers.utils.hexlify(concatBytes));
