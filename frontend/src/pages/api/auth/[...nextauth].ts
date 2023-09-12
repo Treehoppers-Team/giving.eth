ec56c14a4872eb631d96e532a3445c27c0dbaae3
advait <advait001@e.ntu.edu.sg>
2023-09-12 13:43:37 +0800

    Update [...nextauth].ts

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -9,5 +9,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
+  secret: process.env.SECRET,
 };
 export default NextAuth(authOptions);
