d649d1722cc2b79213049adb927908b9e3118a3b
rilwan99 <rilwanwild@gmail.com>
2023-09-12 12:08:44 +0800

    test

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
+export const runtime = "edge";
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
